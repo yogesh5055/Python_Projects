9fd945e1c0b154ebd3a60dd5ac32b5280d18283b
Yogeshwaran T <115524423+yogesh5055@users.noreply.github.com>
2024-10-05 15:39:45 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,1 +1,6 @@
-
+''' This is my first project in 100Days Python Chellange.This program will gives you a band name based on a input you're given 
+to this program '''
+print("----------------Wecome to the Band Name Generator-----------------")
+city_name=input("What's the name of the city you grew up in?")
+pet_name=input("what's your pet name?")
+print("You Got it!\nYour Band Name Could be "+city_name+" "+pet_name)
