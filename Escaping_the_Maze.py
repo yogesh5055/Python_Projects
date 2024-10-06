7b4029774cc700e0602bc5cb9b8c0b2ded796498
Yogeshwaran T <115524423+yogesh5055@users.noreply.github.com>
2024-10-06 22:23:59 +0530

    Create Escaping_the_Maze.py

---
+++ Escaping_the_Maze.py
@@ -0,0 +1,13 @@
+'''
+This project full website based if want to check out this 
+follow this link ----> https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json 
+'''
+while not at_goal():
+    if wall_in_front():
+        turn_left()
+    if front_is_clear():
+        move()
+    if right_is_clear():
+        turn_left()
+        turn_left()
+        turn_left()
