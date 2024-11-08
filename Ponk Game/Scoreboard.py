d375b06789e8134c9c225fb620f528d5a08ce6e9
Yogeshwaran T <115524423+yogesh5055@users.noreply.github.com>
2024-11-08 21:54:17 +0530

    Create Scoreboard.py

---
+++ Ponk Game/Scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.l_score=0
+        self.r_score=0
+        self.score_board()
+    def score_board(self):
+        self.clear()
+        self.goto(-100,200)
+        self.write(self.l_score,align="center",font=("Courier",60,"normal"))
+        self.goto(100,200)
+        self.write(self.r_score,align="center",font=("Courier",60,"normal"))
+
+    def l_point(self):
+        self.l_score+=1
+        self.score_board()
+
+    def r_point(self):
+        self.r_score+=1
+        self.score_board()
