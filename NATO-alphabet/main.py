5b422cc1a83b44e5c5c93b812becc5ca598c1359
Yogeshwaran T <115524423+yogesh5055@users.noreply.github.com>
2024-12-01 11:30:29 +0530

    Update main.py

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,9 +1,15 @@
 import pandas as pd
 data=pd.read_csv("nato_phonetic_alphabet.csv")
 new_dict={key.letter:key.code for (index,key) in data.iterrows() }
-user_input =input("Enter a word :").upper()
-ans=[new_dict[letter]  for letter in user_input ]
-print(ans)
-
+def generate_word():
+  user_input =input("Enter a word :").upper()
+  try:
+     ans=[new_dict[letter]  for letter in user_input ]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+    generate_word()
+  else:
+    print(ans)
+generate_word()
 
 
